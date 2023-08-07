9ba515b9f0002e6983aa734812a5a1ed4b0213be
Lukasz Szafron <lukszafron@gmail.com>
2023-08-07 20:03:50 +0200

    Update

--- vep.comparison.r
+++ vep.comparison.r
@@ -3,7 +3,15 @@ cat("Program path:", unlist(strsplit(grep(commandArgs(), pattern = "file=", valu
 
 arguments2 <- commandArgs(trailingOnly = T)
 
-if(length(arguments2) != 8) {stop("This program requires eight arguments. The first one is the destination folder, the second is the number of threads to use, the third should point to the CSV file used in the vep.r analysis, the fourth is the grouping variable, the fifth is the independent factor used, the sixth determines if the samples are paired, the seventh indicates the pair indicator column, while the eighth determines whether the FDR correction should be performed.")}
+if(length(arguments2) != 8) {stop("This program requires eight arguments.
+                                  The first one is the destination folder,
+                                  the second is the number of threads to use,
+                                  the third should point to the CSV file used in the vep.r analysis,
+                                  the fourth is the grouping variable,
+                                  the fifth is the independent factor used,
+                                  the sixth determines if the samples are paired,
+                                  the seventh indicates the pair indicator column,
+                                  while the eighth determines whether the FDR correction should be performed.")}
 arguments2
 
 library(doMC)
